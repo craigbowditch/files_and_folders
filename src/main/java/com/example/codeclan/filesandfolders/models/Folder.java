decb12f4be449e66bcf6a725e7c96ecd6eb66dd4
Craig Bowditch <craigbowditch@hotmail.com>
2018-10-09 19:08:10 +0100

    Creates models for File Folder and User.

---
+++ src/main/java/com/example/codeclan/filesandfolders/models/Folder.java
@@ -0,0 +1,55 @@
+package com.example.codeclan.filesandfolders.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "folder")
+public class Folder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "folder", fetch = FetchType.LAZY)
+    private List<File> files;
+
+    public Folder(String title) {
+        this.title = title;
+        this.files = new ArrayList<>();
+    }
+
+    public Folder() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<File> getFiles() {
+        return files;
+    }
+
+    public void setFiles(List<File> files) {
+        this.files = files;
+    }
+}
